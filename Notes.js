8955736030430e787f6dcfe0d7f7ca1f206e1bbc
johnson-rl <johnson.rl@gmail.com>
2017-02-19 00:02:50 -0800

    removed lecture time calc from notes

--- Notes.js
+++ Notes.js
@@ -1,14 +1,3 @@
-// Time remaining calc
-// ----------------------
-
-// min = 5+7+9+6+8+7+13+10+10+9+6+6+9+6+10+7+7+4
-// console.log(min)
-// sec = 10 + 57 + 56 + 28 + 12 + 27 + 2 + 15 + 55 + 53 + 53 + 7 + 54 + 0 + 37 + 59 + 16 + 59
-// console.log(sec)
-// min += Math.floor(sec/60)
-//
-// console.log(min + " Minutes " + (sec%60) + " Second")
-
 /////////////////
 //    Notes    //
 /////////////////
